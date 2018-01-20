1bbc31da30d173c6248eac8c0b950ca13127cdf8
ROHROCK <rohithamster@gmail.com>
2018-01-20 15:30:34 +0530

    Added the First_Assignment

--- Booktree.cpp
+++ Booktree.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string.h>
-        
+      
 using namespace std;
 
 struct node
@@ -15,7 +15,7 @@ class BST
 {
 		node* root ;
 		int count;
-		
+
 	public:
 		void init();
 		node* createNode(int);
@@ -38,11 +38,11 @@ node* BST::createNode(int d)
 	node * tmp = new node;
 	tmp->data =d;
 	tmp->left = tmp->right = NULL;
-	
+
 	count++;
 	return tmp;
 }
-	
+
 
 void BST::insert( node * r, int data )
 {
@@ -57,16 +57,16 @@ void BST::insert( node * r, int data )
 		{
 			if( r->left == NULL)
 				r->left = createNode(data);
-				
+
 			else
 				insert(r->left, data);
 		}
-	
+
 		else if(data >= r->data)
 		{
 			if(r->right == NULL)
-				r->right = createNode(data); 	 
-			
+				r->right = createNode(data);
+
 			else
 				insert(r->right, data);
 		}
@@ -79,48 +79,48 @@ void BST::display(node * r)
 {
 	if(r == NULL)
 		return;
-		
+
 	cout<<r->data<<"  ";
-	
+
 	display(r->left);
 	display(r->right);
 }
-	
+
 int BST::nodeCount(){return count;}
-	
-	
+
+
 bool search(node * r , int sv)
 {
 	if(r == NULL)
 		return false;
-		
+
 	if(r->data == sv)
 		return true;
-	
+
 	else
 	{
 		if( r->data > sv )
 		{
 			if( r->left == NULL)
 				return false;
-				
+
 			else
 				return search(r->left, sv);
 		}
-	
+
 		else if(sv >= r->data)
 		{
 			if(r->right == NULL)
-				return false; 	 
-			
+				return false;
+
 			else
 				return search(r->right, sv);
 		}
 	}
-	
+
 	return false;
 }
-		
+
 int BST::minVal(node* r)
 {
 	if(r == NULL)
@@ -130,20 +130,20 @@ int BST::minVal(node* r)
 			r = r->left;
 	return r->data;
 }
-	
+
 int main()
 {
 	BST b;
 	int d;
 	b.init();
-	
+
 	while(1)
 	{
 		cout<<"\n\n*****************************************************";
 		cout<<"\nMENU\n1.insert\n2.display\n3.node count\n4.search \n5. min val \nchoice : ";
 		int ch;
 		cin>>ch;
-		
+
 		switch(ch)
 		{
 			case 1:
@@ -151,7 +151,7 @@ int main()
 					cin>>d;
 					b.insert(b.getRoot(), d);
 					break;
-					
+
 			case 2:
 					cout<<"Tree Data in prefix : ";
 					b.display(b.getRoot());
@@ -166,16 +166,16 @@ int main()
 						cout<<"found :) ";
 					else
 						cout<<"\nNot found !";
-					break;		
+					break;
 			case 5:
 					d=b.minVal(b.getRoot());
 					if(d==-1)
 					cout<<"\nTree Empty !";
-					
+
 					else
 					cout<<"\nMin val :"<<d;
 		}
 	}
-	
+
 	return 0;
-}	
+}
