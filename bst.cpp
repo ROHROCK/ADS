7abaa134996daebba517e55ad29faffab4453b77
Rohit <rohitsa40@gmail.com>
2018-01-27 15:04:08 +0530

    assignment 2 complete code

--- bst.cpp
+++ bst.cpp
@@ -29,19 +29,31 @@ public:
 
 class bst {
   int highestDepth;
-  node *Head;
+  node *Head,*revHead;
 public:
   node* getHead(){ return Head;}
-  bst(){Head = NULL;}
+  node* getRevHead(){return revHead;}
+  bst(){revHead = Head = NULL;}
 
-  //Main logic to add the bst
-  void addNode(int data)
+  //Main logic to add the bst ..when flag value is 0 then add the main else if 1 then add to the copy tree
+  void addNode(int data,int f)
   {
       node *temp = initializeNode(data);
-      node *child = Head,*parent = new node;
-      if (Head == NULL) {
-        Head = temp;
-      }else
+      node *child;
+      node *parent = new node;
+      if(f == 0){
+        cout<<"Main Tree"<<endl;
+        child = Head; //this is the main tree
+      }
+      else if(f == 1){
+        child = revHead; //this is the copy of the tree
+        cout<<"Copy Tree"<<endl;
+      }
+      if (f == 0 && Head == NULL)
+          Head = temp;
+      else if(f == 1 && revHead == NULL)
+          revHead = temp;
+      else
       {
           while(child != NULL)
           {
@@ -61,15 +73,14 @@ public:
    //Code for print in order
    void print(struct node* node)
  {
-      if (node == NULL)
+      if (node == NULL){
            return;
+      }
 
       /* first recur on left child */
       print(node->left);
-
       /* then print the data of node */
       cout<<node->data<<" ";
-
       /* now recur on right child */
       print(node->right);
  }
@@ -90,10 +101,47 @@ public:
    for (int i = 0; i < no; i++) {
      cout<<"Enter the data: "<<endl;
      cin>>data;
-     addNode(data);
+     addNode(data,0); //add to main tree
+     addNode(data,1);//add to copy tree
    }
   }
 
+  void swap()
+  {
+    //To find the longestPath
+    if(Head == NULL)
+      return ;
+
+    std::queue<node *> q;
+
+    q.push(revHead);
+    int height = 0;
+
+    while (1) {
+      int nodeCount = q.size();
+      if(nodeCount == 0)
+        return;
+      else
+        height++;
+
+        node *temp;
+        node *ptr;
+      //Remove nodes at this level and add the node of next level
+      while(nodeCount > 0)
+      {
+        temp = q.front();
+        q.pop();
+
+        temp = swapPointers(temp);
+        //push its children
+        if(temp->left != NULL)
+          q.push(temp->left);
+        if(temp->right != NULL)
+          q.push(temp->right);
+        nodeCount--;
+       }
+      }
+  }
   int longestPath(){
     //To find the longestPath
     if(Head == NULL)
@@ -126,7 +174,35 @@ public:
        }
       }
 }
-  void searchValue(){}
+  void searchValue(){
+    int key;
+    cout<<"Enter the key you want to search: "<<endl;
+    cin>>key;
+    //code to serach the value in the tree and print the parent
+    if(Head->data == key){
+      cout<<"The data is root value hence no parent.."<<endl;
+      return;
+    }
+
+    node* child = Head, *parent;
+    int flag = 0;
+    while(child != NULL)
+    {
+        parent = child;
+        if (key > child->data)
+          child = child->right;
+        else
+          child = child->left;
+
+        if(child != NULL && child->data == key){
+           flag = 1;
+           cout<<"The key is found ! "<<key<<" The parent is: "<<parent->data<<endl;
+           break;
+        }
+    }
+    if(flag == 0)
+      cout<<"The key is not present in the tree !"<<endl;
+  }
   void searchMin(){
     //code to search the min node in the tree
     node *temp = new node;
@@ -137,7 +213,13 @@ public:
     }
     cout<<"The minimum value of the tree is: "<<temp->data<<endl;
   }
-  void swapPointers(){}
+  node* swapPointers(node *temp){
+      node* newnode = new node;
+      newnode->left = temp->left;
+      temp->left = temp->right;
+      temp->right = newnode->left;
+      return temp;
+  }
 
 };
 
@@ -146,28 +228,30 @@ int main(){
   int ch,data=0;
   char status;
   do {
-    /* code */
+    system("clear");
     cout<<"*************MENU************"<<endl;
     cout<<"1.Create the BST"<<endl; //DONE
     cout<<"2.Add node to the BST"<<endl; //DONE
     cout<<"3.Find number of nodes in longest path: "<<endl; //NOT YET
     cout<<"4.Minimum data in the tree"<<endl; //DONE
-    cout<<"5.Swap the pointers in the tree"<<endl; //NOT YET
+    cout<<"5.Swap the pointers in the tree"<<endl; //DONE
     cout<<"6.Search a value in the tree"<<endl; //NOT YET
     cout<<"7.Print the tree"<<endl;//DONE
-    cout<<"8.Exit"<<endl;
+    cout<<"8.Print the Reverse tree"<<endl;//DONE
+    cout<<"9.Exit"<<endl;
     cout<<"******************************"<<endl;
     cout<<"Enter your choice: "<<endl;
     cin>>ch;
     switch (ch) {
-      case 8: exit(EXIT_SUCCESS);
+      case 9: exit(EXIT_SUCCESS);
       case 1: obj.createBST(); break;
-      case 2: cout<<"Enter the data: "<<endl; cin>>data; obj.addNode(data); break;
+      case 2: cout<<"Enter the data: "<<endl; cin>>data; obj.addNode(data,0); break;
       case 3: cout<<"The number of nodes in longest path: "<<obj.longestPath()<<endl; break;
       case 4: obj.searchMin(); break;
-      case 5: obj.swapPointers(); break;
+      case 5: obj.swap(); break;
       case 6: obj.searchValue(); break;
       case 7: obj.print(obj.getHead()); break;
+      case 8: obj.print(obj.getRevHead()); break;
 
       default: cout<<"Entered value is invalid !"<<endl;
     }
