4b2e8b6255f1bf0a0c90e09a17ce7b0a5b41f03a
Rohit <rohitsa40@gmail.com>
2018-02-19 21:10:07 +0530

    e

---
+++ conversion.cpp
@@ -0,0 +1,64 @@
+/*For given expression eg. a-b*c-d/e+f construct inorder sequence and traverse it using
+postorder traversal(non recursive).*/
+#include<iostream>
+#include<stdlib.h>
+#include<stack.h>
+#define MAX 25
+using namespace std;
+
+class node{
+  string data ;
+  int operand; //if it is operand(any digit) then it should be 1 if it is operator(+) then 0
+  node *left;
+  node *right;
+  node(){
+    data = "";
+    operand = 1;
+    left = right = null;
+  }
+};
+class convert{
+  node *head;
+  stack<node> s;
+  string infix;
+public:
+   void getInfix()
+   {
+     cout<<"Enter the infix expression: "<<endl;
+     cin>>infix;
+   }
+   //convert infix to prefix and return it
+   string prefix(string infix)
+   {
+      /*
+    Step 1: Reverse the infix expression i.e A+B*C will become C*B+A. Note while reversing each ‘(‘ will become ‘)’ and each ‘)’ becomes ‘(‘.
+    Step 2: Obtain the postfix expression of the modified expression i.e CB*A+.
+    Step 3: Reverse the postfix expression. Hence in our example prefix is +A*BC.
+*/
+   }
+};
+int main()
+{  int ch,data=0;
+  char status;
+  do {
+    system("clear");
+    cout<<"*************MENU************"<<endl;
+    cout<<"1.Enter infix expression to convert expression tree"<<endl; //DONE
+    cout<<"2.Traverse using postorder traversal"<<endl; //DONE
+    cout<<"3.Exit"<<endl;
+    cout<<"******************************"<<endl;
+    cout<<"Enter your choice: "<<endl;
+    cin>>ch;
+    switch (ch) {
+      case 3: exit(EXIT_SUCCESS);
+      case 1:
+      break;
+      case 2:
+      break;
+      default: cout<<"Entered value is invalid !"<<endl;
+    }
+    cout<<"Do you want to continue ?[Y/N]"<<endl;
+    cin>>status;
+  } while(status == 'y' || status == 'Y');
+  return 0;
+}
