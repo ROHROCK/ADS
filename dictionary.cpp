2d2f7ff1e6ae9b37063288346fe13a1729127a73
Rohit <rohitsa40@gmail.com>
2018-03-19 18:45:41 +0530

    added dictionary.cpp file

--- dictionary.cpp
+++ dictionary.cpp
@@ -1,146 +1,169 @@
 #include<iostream>
 #include<stdlib.h>
-#define SIZE 101
 using namespace std;
-//find the hash using ASCII value
-class Key{
-public:
-  int key; //acts as the index and as a key both
-  Key *ptr;
-  Key(){ ptr = NULL;}
+
+class node{
+  public:
+  string word;
+  string meaning;
+  node *next;
+  node(){
+    next = NULL;
+  }
 };
-
-class hashing //where the hash table exits
-{
-  string hashTable[SIZE];
-  int key;
-  int skey;
-  Key *nameptr;
-  string value;
+class hashClass{
+  node *hashTable;
+  int s;
 public:
-  hashing(int size){
-    nameptr = new name[size];
-  }
-  int isUnique()
-  {
-    //code to create a binary search to check if key is duplicate or not
-  }
-  void getKey(){
-    cout<<"Enter the key: "<<endl;
-    cin>>n;
-    cout<<"Enter the data: "<<endl;
-    cin>>value;
-  //  if(key.isUnique())
-//    {
-      //cout<<"Enter the Key to be inserted in the hash table: "<<endl;
-      //cin>>key;
-
-//    }
-    //else
-//    {
-      //cout<<"Please enter a unique key !"<<endl;
-  //  }
-  }
-  //hash code taken from online ---
-  int hashstring(unsigned char *str)
-  {
-    int hash = 5381;
-    int c;
-
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-    return hash;
-  }
-  // int hash()
-  // {
-  //   char k = n[0];
-  //   return (firstCharASCII % SIZE-1); //works for lower case alphabets (range less than 100)
-  // }
-  void insertIntoHashTable(){
-    getKey();
-    int location = hashString(n);
-    cout<<"Location: "<<location;
-    //location = location%100;
-    cout<<key<<endl;
-    if(hashTable[location].empty())
+    hashClass(int size){
+      s = size;
+      hashTable = new node[size];
+    }
+    node* createNode(string word , string meaning) {
+      node *temp = new node();
+      temp->word = word;
+      temp->meaning = meaning;
+      return temp;
+    }
+    int hashFunction(string name)
     {
-      nameptr[location].name = this->n;
-      nameptr[location].loc = location;
-      hashTable[location] = key;
-      cout<<"Number Successfully Entered Into the Hash Table !"<<endl;
-    }else{
-      //collision occured
-      while(location < SIZE -1)
-        if(hashTable[++location].empty())
-          break;
-
-      if(location == SIZE - 1)
-      {
-        cout<<"Sorry the Hash table is FULL !"<<endl;
-        return;
-      }
-      nameptr[location].name = this->n;
-      nameptr[location].loc = location;
-      hashTable[location] = key;
-      cout<<"Number Successfully Entered Into the Hash Table !"<<endl;
+        int ASCIIAddition= 0;
+        for(int i = 0 ; i < name.length() ;  i++)
+        {
+          ASCIIAddition += name[i];
+        }
+        return ASCIIAddition%s;
     }
-  }
+    void insertIntoHashTable()
+    {
+        string word,meaning;
+        cout<<"Enter the word: "<<endl;
+        cin>>word;
+        cout<<"Enter the meaning: "<<endl;
+        cin>>meaning;
+        int location = hashFunction(word);
+        if(hashTable[location].word.empty())
+        {
+          //Here the location is empty .. just place it in the hash table
+          hashTable[location].word = word;
+          hashTable[location].meaning = meaning;
+          hashTable[location].next = NULL;
+        }else{
+          //Collision ocurred !
+          node *temp = createNode(word,meaning); //create a node
+          node *ptr = &hashTable[location];
+          while(ptr->next != NULL){
+            ptr = ptr->next;
+          }
+          ptr->next = temp;
+        }
+    }
 
-  void displayHashTable()
-  {
-    cout<<"ID---------- VALUE-----"<<endl;
-    for(int i = 0 ; i < SIZE ; i++)
-      cout<<i<<"\t"<<hashTable[i]<<endl;
-  }
 
-  void searchKey()
-  {
-    string name;
-    cout<<"Enter the name: "<<endl;
-    cin>>name;
-    for(int i = 0 ; i < SIZE ; i++){
-      if(name == (nameptr[i].name))
-      {
-        cout<<"Found !"<<endl<<"Number: "<<hashTable[nameptr[i].loc];
-        return;
+    void deleteWord(){
+      string wordToBeDeleted;
+      cout<<"Enter the word to be deleted !"<<endl;
+      cin>>wordToBeDeleted;
+      int location = hashFunction(wordToBeDeleted);
+      if(wordToBeDeleted == hashTable[location].word){
+        //if word is in Hash table default;
+        if(!hashTable[location].next){
+          //only one word exits...
+          cout<<"SINGLE ENTRY"<<endl;
+          hashTable[location].word = "";
+          hashTable[location].meaning = "";
+        }else{
+          //chain exits in that particular location and there might be a word in the chain
+          cout<<"DOUBLE ENTRY !"<<endl;
+          node *ptrprev = NULL;
+          node* ptr = &hashTable[location];
+          while(ptr != NULL) //traverse the chain ...
+          {
+              if(ptr->word == wordToBeDeleted) //Then word is found !
+                break;
+              ptrprev = ptr;
+              ptr = ptr->next;
+          }
+          if(ptr == NULL){ //if  word is not found !
+            cout<<"No such word exist !"<<endl;
+            return;
+          }
+          if(ptr->next != NULL) //when the word is in the middle
+          {
+            ptrprev->next = ptr->next;
+            free(ptr);
+            return;
+          }
+          ptrprev->next = NULL; //when the word is at the end
+          free(ptr);
+        }
       }
     }
-    cout<<"NOT FOUND !"<<endl;
+
+    void display(){
+      string keyWord;
+      cout<<"Enter the word to display: "<<endl;
+      cin>>keyWord;
+      int location = hashFunction(keyWord);
+      if(hashTable[location].word.empty())
+      {
+        //does not exist !
+        cout<<"The word does not exist !"<<endl;
+      }else if(!hashTable[location].word.empty()){
+        cout<<"The word exist !"<<endl;
+        if(hashTable[location].word == keyWord)
+        {
+          cout<<"The meaning is: "<<hashTable[location].meaning<<endl;
+          return;
+        }else{
+          //search in middle
+          node *ptr = &hashTable[location];
+          while(ptr != NULL){
+            if(ptr->word == keyWord)
+              break;
+
+            ptr = ptr->next;
+        }
+        if(ptr->word.empty()){
+            cout<<"No such word exist !"<<endl;
+            return;
+        }else{
+          cout<<"The meaning is: "<<ptr->meaning<<endl;
+        }
+      }
+    }
   }
 };
 
 int main()
 {
-  hashing obj;
+  hashClass object(100);
   int ch;
   char status;
   do{
-    system("clear");
-    cout<<"***********MENU**************"<<endl;
-    cout<<"1.Enter the phone number: "<<endl;
-    cout<<"2.Display the hash table : "<<endl;
-    cout<<"3.Search the phone number: "<<endl;
+    cout<<"1.Insert word into the dictionary"<<endl;
+    cout<<"2.Delete word from the dictionary"<<endl;
+    cout<<"3.Display the hash table"<<endl;
     cout<<"4.Exit"<<endl;
-    cout<<"*****************************"<<endl;
+    cout<<"********************************"<<endl;
+    cout<<"Enter your choice : "<<endl;
     cin>>ch;
     switch (ch) {
       case 4:
         exit(EXIT_SUCCESS);
       case 1:
-        obj.insertIntoHashTable();
+        object.insertIntoHashTable();
       break;
       case 2:
-        obj.displayHashTable();
+        object.deleteWord();
       break;
       case 3:
-        obj.searchKey();
-      break;
-
+      object.display();
+        break;
       default:
-        cout<<"Entered Data is invalid !"<<endl;
+        cout<<"Invalid choice entered !"<<endl;
     }
-    cout<<"Do you want to continue ? [Y/N]"<<endl;
+    cout<<"Do you want to continue ?[Y/N]"<<endl;
     cin>>status;
   }while(status == 'Y' || status == 'y');
   return 0;
